fead52b4a3165756ef6409c70b4cfb0b71d79eb8
CarlosVG <carlosvelasquezgil@gmail.com>
2021-10-23 15:48:15 -0500

    Update layout.blade.php

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -35,6 +35,14 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="/">Inicio</a>
                     </li>
+                    <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Gestiones</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="{{route('terceros.index')}}">Terceros</a>
+             {{--  <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a> --}}
+            </div>
+          </li>
                     {{-- <li class="nav-item">
           <a class="nav-link" href="#">Features</a>
         </li>
